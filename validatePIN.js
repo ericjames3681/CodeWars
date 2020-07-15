a3da1b9c39e7f5df5f3a6ee3dbae7a2b94dc3115
apple <apple@MacBook.home>
2020-07-14 21:05:52 -0500

    validatePIN...

---
+++ validatePIN.js
@@ -0,0 +1,15 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain
+// anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// validatePIN("1234") === true
+// validatePIN("12345") === false
+// validatePIN("a234") === false
+
+function validatePIN(pin) {
+  let re = /^(\d{4}|\d{6})$/;
+  return re.test(pin);
+}
+
+console.log(validatePIN("3142"));
