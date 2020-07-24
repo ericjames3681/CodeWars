16be023545946eca90bca1131db940ea9e59bf03
Eric Peterson <ericjamesp.2009@gmail.com>
2020-07-24 09:50:29 -0500

    reverseWords...

--- expandedForm.js
+++ expandedForm.js
@@ -1,3 +1,10 @@
+// You will be given a number and you will need to return it as a string
+// in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+
 function expandedForm(num) {
   let arr = num.toString().split("").reverse();
   let result = [];
